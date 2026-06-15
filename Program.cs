08206c64584169961feec95817ef72a40c115be8
DanBanan02 <dan.pg26.kodehode@gmail.com>
2026-06-15 10:05:06 +0200

    OverLoad 0.1 ( Local Methods. Error not supporting overload.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
-// test commit...
+﻿
+// Method overloads
+
+// Overload in a build methods
+ Console.WriteLine("Hello!"); // WriteLine can take a string (This is one overload)
+
+ Console.WriteLine(1234); // another overload (numbers)
+
+ Console.WriteLine('A'); // Another Overload ( Characters )
+
+ static void Greet(string data)
+{
+    Console.WriteLine(data);
+}
+
+
+ static void Greet(int data)
+{
+    Console.WriteLine(data);
+}
+
+
+Greet("Hi There");
+Greet(1234);
---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# Backend uke 3
+
+- Methods: Overloading
+- Classes: Basic Intro and 
+- instances methods.
+- etc.
