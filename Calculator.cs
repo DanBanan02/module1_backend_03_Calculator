d6863802bda23c987f2f7bd8f163558462a6d6a3
DanBanan02 <dan.pg26.kodehode@gmail.com>
2026-07-03 11:11:54 +0200

    Calcus ( Slight Improvments )

--- Calculator.cs
+++ Calculator.cs
@@ -1,21 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Calculator
 {
     class Calcus
-
     {
         static async Task Main()
         {
-
-
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("Loading: 0%");
 
-            for (int i = 1; i <= 100; i += 1)
+            for (int i = 1; i <= 100; i++)
             {
                 Random random = new Random();
-                int number = random.Next(1,2);
+                int number = random.Next(1, 2);
 
                 await Task.Delay(number + number + number);
 
@@ -24,43 +22,35 @@ namespace Calculator
             }
 
             Console.ResetColor();
-            Console.WriteLine();
+            Console.WriteLine("");
             Console.WriteLine("");
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("=== Calculator ===");
             Console.ResetColor();
 
-            // Calculate functions together = Result
             Console.WriteLine("");
             Console.WriteLine("");
             Console.Write("Calculate Task: ");
             string? input = Console.ReadLine();
 
-            int Ioo = 100;
             char selector = ' ';
-            
+
             if (input!.Contains('+'))
                 selector = '+';
-
             else if (input.Contains('-'))
                 selector = '-';
-
             else if (input.Contains('*'))
                 selector = '*';
-
             else if (input.Contains('/'))
                 selector = '/';
-
             else if (input.Contains('%'))
                 selector = '%';
-
             else if (input.Contains('='))
                 selector = '=';
-
             else
             {
-                Console.WriteLine("Invalid Calculation Character...");
+                Console.WriteLine("Invalid Character...");
                 return;
             }
 
@@ -68,88 +58,75 @@ namespace Calculator
 
             double num1 = Convert.ToDouble(Numb[0]);
             double num2 = Convert.ToDouble(Numb[1]);
-            
+
+            double result = Calculate(num1, num2, selector);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+
+            foreach (char Number in result.ToString())
+            Console.WriteLine("Converting: " + Number);
+            Console.ResetColor();
 
 
-            // Tells the number to begin at = Zero
-            double result =0;
+            string letterResult = " ";
 
-            // Diffrent Fucntions
+            foreach (char Numbers in result.ToString())
+            {
+                switch (Numbers)
+                {
+                    case '1': letterResult += "A "; break;
+                    case '2': letterResult += "B "; break;
+                    case '3': letterResult += "C "; break;
+                    case '4': letterResult += "D "; break;
+                    case '5': letterResult += "E "; break;
+                    case '6': letterResult += "F "; break;
+                    case '7': letterResult += "G "; break;
+                    case '8': letterResult += "H "; break;
+                    case '9': letterResult += "I "; break;
+                    case '0': letterResult += "J "; break;
+                    case '.': letterResult += ". "; break;
+                }
+            }
+
+            Console.WriteLine($"Result:       {letterResult}");
+
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        static double Calculate(double num1, double num2, char selector)
+        {
             switch (selector)
             {
-            // Adds plus function
-            case '+':
-                result = num1 + num2;
-                break;
-            
-            // Adds minus function
-            case '-':
-                result = num1 - num2;
-                break;
+                case '+':
+                    return num1 + num2;
 
-            // Adds multiplication function
-            case '*':
-                result = num1 * num2;
-                break;
+                case '-':
+                    return num1 - num2;
 
-            // Adds divide function
-            case '/':
-                if (num2 == 0)
-                {
-                    Console.WriteLine("Invalid Function");
-                    return;
-                }
-                result = num1 / num2;
-                break;
-            
+                case '*':
+                    return num1 * num2;
 
-            // Adds percentage function
-            case '%':
-                    result = num1 + num2 * 100;
-                    break; 
-            
-            case '=':
-                    result = num1 / Ioo / num2;
-                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Invalid Function");
+                        return 0;
+                    }
+                    return num1 / num2;
+
+                case '%':
+                    return num1 + num2 * 100;
+
+                case '=':
+                    return num1 - 100 / num2;
 
                 default:
-            Console.WriteLine("Invalid Input");
-
-            return;  
+                    Console.WriteLine("Invalid Input");
+                    return 0;
             }
-                Console.ForegroundColor=ConsoleColor.Yellow;
-                Console.WriteLine(" ");
-                foreach (char Number in result.ToString())
-                Console.WriteLine("Converting: "+ Number);
-                Console.ResetColor();
-            
-                // Converting Numbers to Letters.
-
-                string letterResult = " ";
-
-                foreach (char Numbers in result.ToString())
-                {
-                    switch (Numbers)
-                    {
-                        case '1': letterResult += "A "; break;
-                        case '2': letterResult += "B "; break;
-                        case '3': letterResult += "C "; break;
-                        case '4': letterResult += "D "; break;
-                        case '5': letterResult += "E "; break;
-                        case '6': letterResult += "F "; break;
-                        case '7': letterResult += "G "; break;
-                        case '8': letterResult += "H "; break;
-                        case '9': letterResult += "I "; break;
-                        case '0': letterResult += "J "; break;
-                    }
-                }
-
-                // Adds all into a result
-                Console.WriteLine($"Result:       {letterResult}");
-
-                Console.WriteLine("\nPress any key to exit...");
-                Console.ReadKey();
-            
-        }    
+        }
     }
-}    
+}
